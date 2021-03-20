6c48f939b5bc2eb3b2738e408cbc7abbfc34a74d
Mohd Amir Ziya <itsamirzia@gmail.com>
2021-03-20 16:12:41 +0530

    Update testam.cs

--- testam.cs
+++ testam.cs
@@ -1,170 +1,288 @@
+'*************************************'
+'* Written by Jeremy Giaco 4/25/2005 *'
+'*************************************'
 
-'
-'---	This VBScript will retrieve the nessasary document data and then run a query against the DW
-'---	returning the AR Balance. It will then populate the new key field AR - RGH AR Balance' and return
-'---	to the workflow. If the AR Balance is equal to 0.00 the document will then exit the workflow. If 
-'---	the AR Balance is not equal to 0.00 this script will load the AR Balance into the new keyword field
-'---	'AR - RGH AR Balance'. Then the document will follow the existing rules
-'
-'---	Vaiable declarations
-'
-Dim strDBDatabase
-Dim strDBPassword
-Dim strDBServer
-Dim strDBUsername
-Dim strConn
-Dim objConn
-Dim objRS
-Dim sql
-'
-Dim strAccountNumber
-Dim strClaimNumber
-Dim strDocId
-Dim strDocumentDate
-Dim strDOS
-Dim strItemNum
-'
-Dim strMonth
-Dim strDay
-Dim strYear	
-Dim strTempDate
-Dim strTempDate2
-Dim strToday
-Dim Month
-Dim Day
-Dim Year
-'
-Dim AR_Balance
-Dim Count
-Dim DaysDiff
-Dim Queue
-'
-Dim bARBalance
-'
-'---	Get current doc and keywords
-'
-Dim myApp
-Set myApp = CreateObject("Onbase.Application")
-'
-Dim myDoc
-Set myDoc = myApp.CurrentDocument
-'
-Dim myKeys
-Set myKeys = myDoc.Keywords
-'
-Dim myUsername
-myUsername = myApp.username
-'
-'Initalize variables
-'
-on error resume next
-count = 0
-'
-'---	Need to save certain fields retrieved from the document for later use
-'
-Do while count < myKeys.Count
-	If myKeys.item(count).name = "AR - Account Number" Then
-		If Len(strAccountNumber) = 0 Then
-			strAccountNumber = RTrim(myKeys.item(count).value)
-		End If
-	End If
-'
- 	If myKeys.item(count).name = "AR - Date of Service" Then
+'This script generates a review letter from an OnBase 835 (EOB)
 
-		If Len(strDOS) = 0 Then
-			strTempDate = myKeys.item(count).value		
-'			
-			strMonth = Mid(strTempDate,1,2)
-			Month = strMonth
-'		
-			strDay = Mid(strTempDate,4,2)
-			Day = strDay
-'			
-			strYear	= Mid(strTempDate,7,4)
-			Year = strYear
-'			
-			strDOS =  Year & "-" & Month & "-" & Day & " 00:00:00.000"	
-		End If
-	End If
-'
-	Count = count + 1
+'Constants for File System Object
+Const ForReading = 1 
+Const ForWriting = 2 
+Const ForAppending = 8 
+
+'Starting text of the reason for request
+Dim sReason
+'InputBox("Please enter detailed reason for request:", _
+'"Reason For Request", _
+sReason = "The written/verbal order indicates a medical necessity of ..."
+
+'OnBase Application object
+Dim oApp
+Set oApp = CreateObject("Onbase.Application")
+
+'OnBase Document Object
+Dim oDoc
+Set oDoc = oApp.CurrentDocument
+
+'OnBase Keyword Object
+Dim oKeywords
+Set oKeywords = oDoc.Keywords
+
+'Get Document Handle
+Dim sDocHandle
+sDocHandle = oDoc.Handle
+
+'Used to loop through document keywords
+Dim iCount
+iCount = 0
+
+Dim sBeneficiary
+Dim sDOS
+Dim sCCN
+Dim sHIC
+Dim sDOD
+Dim sHCPC
+Dim sAcctNum
+Dim sCarrier
+Dim sDate
+sDate = Date()
+
+'Loop through keywords and set strings equal to keyword values
+Do while iCount < oKeywords.Count
+	Dim sKeyName
+	Dim sKeyValue
+	
+	sKeyName = oKeywords(iCount).Name
+	sKeyValue = oKeywords(iCount).Value
+	
+	Select Case sKeyName
+		Case "AR - Carrier Name"
+			sCarrier = sKeyvalue
+		Case "AR - Account Name"
+			sBeneficiary = sKeyValue
+		Case "AR - Date of Service"
+			sDOS = sKeyValue
+		'Case "AR - Claim Number"
+			'sCCN = sKeyValue
+		Case "AR - CCN"
+			sCCN = sKeyValue
+		Case "AR - Patient ID"
+			sHIC = sKeyValue
+		Case "AR - Payment Date"
+			sDOD = sKeyValue
+		Case "AR - Procedure Code"
+			sHCPC = sHCPC & sKeyValue & " "
+		Case "AR - Account Number"
+			sAcctNum = sKeyValue
+	End Select 
+	
+	'Increment Counter	
+	iCount = iCount + 1
 Loop
 
-'MsgBox "Account Number: " & strAccountNumber
-'MsgBox "Date of Service: " & strDOS
+'File System Object
+Dim oFSO
+Set oFSO = CreateObject("Scripting.FileSystemObject")
 
-'
-'---	If there is no account number on the document then do not process it.
-'
-If strAccountNumber <> "" Then
-'
-'
-'---	Using the customer number and date of service from the document, this script will access the Edgepark Data Warehouse 
-'---	and query the table rghdm.dbo.ARFact, sum up the A/R Balance for the date of service and return it to populate the 
-'---	key field 'AR - RGH AR Balance' on the document
-'
-	strDBServer = "EPDW" 
-	strDBUsername = "dw"	
-	strDBPassword = "dw"	
-	strDBDatabase = "EPDW"
-'
-	strConn = "driver={SQL Server};server=" & strDBServer & ";UID=" & strDBUsername & ";PWD=" & strDBPassword & ";database=" & strDBDatabase
-	Set objConn = CreateObject("ADODB.Connection")
-	Set objRS = CreateObject("ADODB.Recordset")
-'
-	objConn.open strConn
-	objRS.ActiveConnection = objConn
-'
-	sql = 	"select SUM(ISNULL(AR_Amount,0)) as ARBalance from dbo.EdgAR (nolock) where AR_Company = '1' and AR_CustomerNo = '" & strAccountNumber & _ 
-		"' and AR_ApplyToDos = '" & strDOS  & "' and AR_InsCode IN (5001,5002,5003,5004,5011,5012,5013,5014)"
-'
-	objRS.Open sql
-	AR_Balance = objRS.fields("ARBalance").value
-'	msgbox sql
-'
-'	msgbox "AR Balance: " & AR_Balance
-'
-'---	Add the newly calculated A/R Balance to the document key. Alway do this regardless of the amount.
-'---	The workflow will check the AR Balance and decide what to do with the document.	
-'
-	If AR_Balance <> "" Then
-		bARBalance = FALSE
-		Count = 0
-		Do while count < myKeys.Count
-			If myKeys.item(count).name = "AR - RGH AR Balance" Then
-				myKeys(count).value = AR_Balance
-				bARBalance = TRUE
-			End If
-			Count = Count + 1 
-		Loop
-		'msgbox bARBalance
-'
-		If (bARBalance = FALSE) Then
-			Call myKeys.AddKeyword("AR - RGH AR Balance",AR_Balance)
-		End If
-'
-		Call myDoc.StoreKeywords()
-'
-	End If
-'
-	If objRS.State = 1 Then
-		objRS.Close
-	End If
-'
-'---	Close connection (if open)
-'
-	If objConn.State = 1 Then
-		objConn.Close
-	End If
-
-        'Moved database object destroy to within if where it was created
-       Set strConn = Nothing
-       Set objRS = Nothing
-       Set objConn = Nothing
-
+'Choose correct template file based on carrier name
+Dim sTemplateName
+If InStr(1,UCase(sCarrier),"NATIONAL HERITAGE",1) > 0 Then
+	sTemplateName = "reviewregiona.rtf"
+ElseIf InStr(1,UCase(sCarrier),"NHIC",1) > 0 Then
+	sTemplateName = "reviewregiona.rtf"
+ElseIf InStr(1,UCase(sCarrier),"REGION A",1) > 0 Then
+	sTemplateName = "reviewregiona.rtf"
+ElseIf InStr(1,UCase(sCarrier),"ADMINASTAR",1) > 0 Then
+	sTemplateName = "reviewregionb.rtf"
+ElseIf InStr(1,UCase(sCarrier),"NATIONAL GOVERNMENT SERVICES",1) > 0 Then
+	sTemplateName = "reviewregionb.rtf"
+ElseIf InStr(1,UCase(sCarrier),"REGION B",1) > 0 Then
+	sTemplateName = "reviewregionb.rtf"
+ElseIf InStr(1,UCase(sCarrier),"NGS",1) > 0 Then
+	sTemplateName = "reviewregionb.rtf"
+ElseIf InStr(1,UCase(sCarrier),"NGS, INC. JURISDICTION B DME MAC",1) > 0 Then
+	sTemplateName = "reviewregionb.rtf"
+ElseIf InStr(1,UCase(sCarrier),"PALMETTO",1) > 0 Then
+	sTemplateName = "reviewregionc.rtf" 
+ElseIf InStr(1,UCase(sCarrier),"CIGNA                C",1) > 0 Then
+	sTemplateName = "reviewregionc.rtf"
+ElseIf InStr(1,UCase(sCarrier),"CIGNA GOVERNMENT SERVICES",1) > 0 Then
+	sTemplateName = "reviewregionc.rtf"
+ElseIf InStr(1,UCase(sCarrier),"REGION C",1) > 0 Then
+	sTemplateName = "reviewregionc.rtf"
+ElseIf InStr(1,UCase(sCarrier),"NAS",1) > 0 Then
+        sTemplateName = "reviewregiond.rtf"
+ElseIf InStr(1,UCase(sCarrier),"NORIDIAN",1) > 0 Then
+        sTemplateName = "reviewregiond.rtf"
+ElseIf InStr(1,UCase(sCarrier),"NORIDIAN ADMINISTRATIVE SERVICES",1) > 0 Then
+        sTemplateName = "reviewregiond.rtf"
+ElseIf InStr(1,UCase(sCarrier),"REGION D",1) > 0 Then
+        sTemplateName = "reviewregiond.rtf"
+ElseIf InStr(1,UCase(sCarrier),"CIGNA                D",1) > 0 Then
+	sTemplateName = "reviewregiond.rtf"
+ElseIf InStr(1,UCase(sCarrier),"REGION D",1) > 0 Then
+	sTemplateName = "reviewregiond.rtf"
+ElseIf InStr(1,UCase(sCarrier),"CIGNA - MEDICARE",1) > 0 Then
+	sTemplateName = "reviewregiond.rtf"
+Else
+	Dim sMessage
+	sMessage = _
+		"Cannot find template letter for this insurance. Please type the letter of the correct Medicare region for this claim:" & vbcrlf & _
+		"A - REGION A" & vbcrlf & _
+		"B - REGION B" & vbcrlf & _
+		"C - REGION C" & vbcrlf & _
+		"D - REGION D"
+		
+	Dim sInput
+	sInput = InputBox(sMessage,"Cannot find template","A")
+	Select Case sInput
+		Case "A" 
+			sTemplateName = "reviewregiona.rtf"
+		Case "B" 
+			sTemplateName = "reviewregionb.rtf"
+		Case "C" 
+			sTemplateName = "reviewregionc.rtf"
+		Case "D" 
+			sTemplateName = "reviewregiond.rtf"		
+		Case Else
+			sTemplateName = "reviewregiona.rtf"	
+	End Select				
 End If
-'
 
-Set myKeys = Nothing
-Set myDoc = Nothing
-Set myApp = Nothing
+'Open template
+Dim oTemplate	
+Set oTemplate = oFSO.OpenTextFile("\\3SG.net\applications\EC500\ONB\PRD_SYS\OnBaseFiles\Forms\MedicareReviews\" & sTemplateName,ForReading,False)
+
+'Populate template string with text of word document
+Dim sTemplate
+sTemplate = oTemplate.ReadAll
+
+'Close word document
+oTemplate.Close
+
+'Replace value placeholder with value
+sTemplate = Replace(sTemplate,"#BENEFICIARY#",sBeneficiary)
+sTemplate = Replace(sTemplate,"#DOS#",sDOS)
+sTemplate = Replace(sTemplate,"#CCN#",sCCN)
+sTemplate = Replace(sTemplate,"#HIC#",sHIC)
+sTemplate = Replace(sTemplate,"#DOD#",sDOD)
+sTemplate = Replace(sTemplate,"#HCPC#",sHCPC)
+sTemplate = Replace(sTemplate,"#REASON#",sReason)
+sTemplate = Replace(sTemplate,"#DATE#",sDate)
+
+'Change name to first_middle_last
+Dim aBeneficiary
+aBeneficiary = Split(sBeneficiary,",")
+
+If UBound(aBeneficiary) > 0 Then
+	Dim sFirstName
+	sFirstName = Trim(aBeneficiary(1))
+End If
+
+Dim sLastName
+sLastName = Trim(aBeneficiary(0))
+
+sBeneficary = sFirstName & "_" & sLastName
+sBeneficiary = Replace(sBeneficary,",","")
+sBeneficiary = Replace(sBeneficiary," ","_")
+sBeneficiary = Replace(sBeneficiary,"'","")
+
+'Filename of file to output
+Dim sFileName
+sFileName = "ReviewLetter_" & sBeneficiary & "_" & sDocHandle & ".rtf"
+
+'Full path of output file
+Dim sFilePath
+sFilePath = "\\CORP.3SG.COM\ONB\PRD_SRC\Billing\MedicareReviews\" & sFileName
+
+'Declare oFile
+Dim oFile
+
+If oFSO.FileExists(sFilePath) Then
+	iReturn = MsgBox("Letter already exists, would you like to overwrite?",VbYesNo,"Letter already exists")
+	If iReturn = 6 Then
+
+		'Create output file from full path
+		Set oFile = oFSO.OpenTextFile(sFilePath,ForWriting,True)
+
+		'Write populated template to output file
+		Call oFile.Write(sTemplate)
+
+		'Close output file
+		oFile.Close
+
+		iReturn = MsgBox("Letter has been re-created at: " & vbCrLf & sFilePath & vbCrLf & "Would you like to open the file now?",vbYesNo,"Open file?")
+	Else
+		iReturn = MsgBox("Existing letter is located at: " & vbCrLf & sFilePath & vbCrLf & "Would you like to open the existing letter for review?",vbYesNo,"Open file?")
+	End If
+Else
+	'Create output file from full path
+		Set oFile = oFSO.OpenTextFile(sFilePath,ForWriting,True)
+
+		'Write populated template to output file
+		Call oFile.Write(sTemplate)
+
+		'Close output file
+		oFile.Close
+
+		iReturn = MsgBox("Letter has been generated at: " & vbCrLf & sFilePath & vbCrLf & "Would you like to open the file now?",vbYesNo,"Open file?")
+End If
+
+'Open file if it was created...
+If oFSO.FileExists(sFilePath) Then
+	If iReturn = 6 Then
+		Dim sCommand
+		sCommand =  "winword " & chr(34) & sFilePath & chr(34)
+		
+		Dim oShell
+		Set oShell = CreateObject("WScript.Shell")
+		
+		Dim iReturn
+		iReturn = oShell.Run(sCommand,,False)
+		
+		If iReturn <> 0 Then
+			sCommand = "wordpad " & chr(34) & sFilePath & chr(34)
+			iReturn = oShell.Run(sCommand,,False)
+			If iReturn <> 0 Then
+				MsgBox "There was an error opening file, cannot find a suitable program to open it with.",,"Error opening file."
+			End If
+		End If
+	End If
+	
+	'Write index file for DIP process
+	Dim sImportPath
+	sImportPath = "\\CORP.3SG.COM\ONB\PRD_SRC\BKOIMP\MedicareReviews\" & sFileName
+	
+	'Dim sTimeStamp
+	'sTimeStamp = Now()
+	'sTimeStamp = Replace(sTimeStamp,"/","")
+	'sTimeStamp = Replace(sTimeStamp,":","")
+	'sTimeStamp = Replace(sTimeStamp," ","")
+		
+	'See if account number was picked up, if not do not add to index file
+	If sAcctNum <> "" Then
+		Dim oIndexFile
+		Set oIndexFile = oFSO.OpenTextFile("\\CORP.3SG.COM\ONB\PRD_SRC\Billing\MedicareReviews\MedicareReviewIndex.txt",ForAppending,True)	
+		Call oIndexFile.WriteLine(sAcctNum & "," & sImportPath & "," & "AR - EP - Medicare Reviews")
+		oIndexFile.Close
+	Else
+		MsgBox "Account Number is not set on this document, this document will not be imported into OnBase."
+	End If
+'...otherwise, show cannot be found message
+Else
+	MsgBox sFilePath & " cannot be found.",,"File not found"
+End If
+		
+		
+
+
+'Destroy Objects
+'Removed oKeys - No reference to it in script except in the destroy objects
+'Added oKeywords, changed order
+Set oIndexFile = Nothing
+Set oShell = Nothing
+Set oFile = Nothing
+Set oTemplate = Nothing
+Set oFSO = Nothing
+Set oKeywords = Nothing
+Set oDoc = Nothing
+Set oApp = Nothing
